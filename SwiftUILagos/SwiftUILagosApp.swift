0b8061e860e9601207a2b9d6da86a6c5372cc468
Abdallah Mohammed <abdallahabdulzahir@gmail.com>
2025-04-29 21:52:28 +0300

    Initial Commit

---
+++ SwiftUILagos/SwiftUILagosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILagosApp.swift
+//  SwiftUILagos
+//
+//  Created by name on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILagosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
